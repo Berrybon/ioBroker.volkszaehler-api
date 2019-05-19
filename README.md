8071956936886c481ec4b4221a7c226d8fd8f757
Berrybon <berrybon@gmx.de>
2019-05-19 21:07:40 +0200

    Erstveröffentlichung

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1>
+    <img src="admin/volkszaehler-api.png" width="64"/>
+    ioBroker.volkszaehler-api
+</h1>
+
+[![NPM version](http://img.shields.io/npm/v/iobroker.volkszaehler-api.svg)](https://www.npmjs.com/package/iobroker.volkszaehler-api)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.volkszaehler-api.svg)](https://www.npmjs.com/package/iobroker.volkszaehler-api)
+[![Dependency Status](https://img.shields.io/david/Berrybon/iobroker.volkszaehler-api.svg)](https://david-dm.org/Berrybon/iobroker.volkszaehler-api)
+[![Known Vulnerabilities](https://snyk.io/test/github/Berrybon/ioBroker.volkszaehler-api/badge.svg)](https://snyk.io/test/github/Berrybon/ioBroker.volkszaehler-api)
+
+[![NPM](https://nodei.co/npm/iobroker.volkszaehler-api.png?downloads=true)](https://nodei.co/npm/iobroker.volkszaehler-api/)
+
+**Tests:** Linux/Mac: [![Travis-CI](http://img.shields.io/travis/Berrybon/ioBroker.volkszaehler-api/master.svg)](https://travis-ci.org/Berrybon/ioBroker.volkszaehler-api)
+Windows: [![AppVeyor](https://ci.appveyor.com/api/projects/status/github/Berrybon/ioBroker.volkszaehler-api?branch=master&svg=true)](https://ci.appveyor.com/project/Berrybon/ioBroker-volkszaehler-api/)
+
+## volkszaehler-api adapter for ioBroker
+
+Adapter for connecting ioBroker and Volkszähler
+
+## Changelog
+
+### 0.0.1
+* (Berrybon) initial release
+
+## License
+MIT License
+
+Copyright (c) 2019 Berrybon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ admin/words.js
@@ -0,0 +1,71 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+
+    'volkszaehler-api adapter settings': {
+        'en': 'Adapter settings for volkszaehler-api',
+        'de': 'Adaptereinstellungen für volkszaehler-api',
+        'ru': 'Настройки адаптера для volkszaehler-api',
+        'pt': 'Configurações do adaptador para volkszaehler-api',
+        'nl': 'Adapterinstellingen voor volkszaehler-api',
+        'fr': "Paramètres d'adaptateur pour volkszaehler-api",
+        'it': "Impostazioni dell'adattatore per volkszaehler-api",
+        'es': 'Ajustes del adaptador para volkszaehler-api',
+        'pl': 'Ustawienia adaptera dla volkszaehler-api',
+        'zh-cn': 'volkszaehler-api的适配器设置'
+    },
+
+    'URL or IP to Volkszähler': {
+        'en': 'URL or IP to Volkszähler',
+        'de': 'URL oder IP zum Volkszähler',
+        'ru': 'URL или IP-адрес Volkszähler',
+        'pt': 'URL ou IP para Volkszähler',
+        'nl': 'URL of IP naar Volkszähler',
+        'fr': 'URL ou IP vers Volkszähler',
+        'it': 'URL o IP a Volkszähler',
+        'es': 'URL o IP a Volkszähler',
+        'pl': 'URL lub IP do Volkszähler',
+        'zh-cn': 'Volkszähler的URL或IP'
+    },
+
+    'Path to middleware': {
+        'en': 'Path to middleware',
+        'de': 'Pfad zur Middleware',
+        'ru': 'Путь к промежуточному программному обеспечению',
+        'pt': 'Caminho para o middleware',
+        'nl': 'Pad naar middleware',
+        'fr': 'Chemin vers le middleware',
+        'it': 'Percorso del middleware',
+        'es': 'Camino al middleware',
+        'pl': 'Ścieżka do oprogramowania pośredniego',
+        'zh-cn': '中间件的路径'
+    },
+
+    'Timeout between readings': {
+        'en': 'Timeout between readings',
+        'de': 'Timeout zwischen den Messungen',
+        'ru': 'Тайм-аут между показаниями',
+        'pt': 'Tempo limite entre leituras',
+        'nl': 'Time-out tussen metingen',
+        'fr': "Délai d'attente entre les lectures",
+        'it': 'Timeout tra letture',
+        'es': 'Tiempo de espera entre lecturas',
+        'pl': 'Limit czasu między odczytami',
+        'zh-cn': '读数之间的超时'
+    },
+
+    'Channels from Volkszähler': {
+        'en': 'Channels from Volkszähler',
+        'de': 'Kanäle von Volkszähler',
+        'ru': 'Каналы от Volkszähler',
+        'pt': 'Canais de Volkszähler',
+        'nl': 'Kanalen van Volkszähler',
+        'fr': 'Chaînes de Volkszähler',
+        'it': 'Canali da Volkszähler',
+        'es': 'Canales de Volkszähler',
+        'pl': 'Kanały z Volkszähler',
+        'zh-cn': '来自Volkszähler的频道'
+    }
+
+};
